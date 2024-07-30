5c66f2dccce813e731a38f3a0333c98bc6f673d2
AlandvMarques <alandvmarques@gmail.com>
2024-07-30 09:54:24 -0300

    melhoria de código

--- aprendizado/Program.cs
+++ aprendizado/Program.cs
@@ -1,4 +1,4 @@
-﻿// Using:
+﻿// Using: / System:
 
 using System;
 using System.IO;
@@ -36,11 +36,123 @@ namespace MeuApp
             sbyte meuSbyte = -127;
             Console.WriteLine(meuSbyte);
 
-            //Número Int
+            //Número Short / Int / Long:
+            short meuShort = 34;
+            Console.WriteLine(meuShort);
+
             int meuInt = 12127;
             Console.WriteLine(meuInt);
-        }
-             
+
+            long MeuLong = 64573;
+            Console.WriteLine(MeuLong);
+
+            //Número Float / Double / Decimal:
+            float meuFloat = 2.300f;
+            Console.WriteLine(meuFloat);
+
+            double meuDouble = 4.690;
+            Console.WriteLine(meuDouble);
+
+            decimal meuDecimal = 34.980m;
+            Console.WriteLine(meuDecimal);
+
+            //Número Boolean:
+            bool meuBoolTrue = true;
+            Console.WriteLine(meuBoolTrue);
+
+            bool meuBoolFalse = false;
+            Console.WriteLine(meuBoolFalse);
+
+            var meuBoolVar = true;
+            Console.WriteLine(meuBoolVar);
+
+            //Char:
+            char meuCharPrimeiro = 'a';
+            Console.WriteLine(meuCharPrimeiro);
+
+            char meuCharSecundario = 'b';
+            Console.WriteLine(meuCharSecundario);
+
+            var meuCharVar = 'c';
+            Console.WriteLine(meuCharVar);
+
+            //String:
+            string meuStringPrimario = "minha";
+            Console.WriteLine(meuStringPrimario);
+
+            string meuStringSecundaria = "string";
+            Console.WriteLine(meuStringSecundaria);
+
+            var meuStringVar = "23.4658";
+            Console.WriteLine(meuStringVar);
+
+            //Var: 
+
+            var meuVarPrimario = 25;
+            Console.WriteLine(meuVarPrimario);
+
+            var meuVarSecundario = true;
+            Console.WriteLine(meuVarSecundario);
+
+            var meuVarTernario = 'd';
+            Console.WriteLine(meuVarTernario);
+
+            var meuVarQuartenario = "name";
+            Console.WriteLine(meuVarQuartenario);
+
+            //Object:
+            object semValor;
+
+            object meuObjectPrimario = 25;
+            Console.WriteLine(meuObjectPrimario);
+
+            object meuObjectSecundario = true;
+            Console.WriteLine(meuObjectSecundario);
+
+            object meuObjectTernario = 'e';
+            Console.WriteLine(meuObjectTernario);
+
+            semValor = "atribuindo";
+            semValor = true;
+            semValor = false;
+            Console.WriteLine(semValor);
+
+            //Null:
+            int? meuNullPrimario = null;
+            Console.WriteLine(meuNullPrimario);
+
+            string? meuNullSecundario = null;
+            Console.WriteLine(meuNullSecundario);
+
+            bool? meuNullTernario = null;
+            Console.WriteLine(meuNullTernario);
+
+            //Alias:
+            int meuAlias = 25; //Alias
+            Console.WriteLine(meuAlias);
+
+            Int32 meuTipo = 25; //Tipo
+            Console.WriteLine(meuTipo);
+
+            //Valores Padrões:
+            string meuValorPadraostring = "";
+            Console.WriteLine(meuValorPadraostring);
+
+            int meuValorPadraoint = 0;
+            Console.WriteLine(meuValorPadraoint);
+
+            float meuValorPadraoFloat = 0;
+            Console.WriteLine(meuValorPadraoFloat);
+
+            decimal meuValorPadraoDecimal = 0;
+            Console.WriteLine(meuValorPadraoDecimal);
+
+            bool meuValorPadraoBool = false;
+            Console.WriteLine(meuValorPadraoBool);
+
+            char meuValorPadraoChar = '\0';
+            Console.WriteLine(meuValorPadraoChar);
+        }  
     }
 }
 
