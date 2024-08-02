7fa94bb0c06f68331ce7713794a64540084b3dde
AlandvMarques <alandvmarques@gmail.com>
2024-08-02 09:28:44 -0300

    Finalizando a primeira versão do projeto StopWatch

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -3,7 +3,7 @@ using System.Threading;
 
 namespace StopWatch 
 {
-    class Program 
+    class StopWatch
     {
         static void Main(string[] args) 
         {
@@ -19,14 +19,37 @@ namespace StopWatch
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            char type = char.Parse(data.Substring(data.Length - 1 ,1));
-            int time = int .Parse(data.Substring(0, data.Length - 1));
+            char type = char.Parse(data.Substring(data.Length - 1,1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
+            int multiplier = 1;
 
-            Console.WriteLine(type);
-            Console.WriteLine(time);
+            if (type == 'm') 
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            PreStart(time * multiplier);
 
         }
 
+        static void PreStart(int time) 
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+            Console.Clear();
+
+            Start(time);
+        }
 
         static void Start(int time) 
         {
@@ -43,6 +66,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("StopWatch finalizado. ");
             Thread.Sleep(2500);
+            Menu();
         }
          
     }
