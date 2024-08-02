c1d7f3c34237f9bc4881b547281ad39442f686c2
AlandvMarques <alandvmarques@gmail.com>
2024-08-01 21:16:24 -0300

    Inserindo algumas configurações no meu StopWatch

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace StopWatch 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Menu();
+        }
+
+        static void Menu() 
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 10 minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1 ,1));
+            int time = int .Parse(data.Substring(0, data.Length - 1));
+
+            Console.WriteLine(type);
+            Console.WriteLine(time);
+
+        }
+
+
+        static void Start(int time) 
+        {
+            int currentTime = 0;
+
+            while (currentTime != time )
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+
+            }
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado. ");
+            Thread.Sleep(2500);
+        }
+         
+    }
+}
